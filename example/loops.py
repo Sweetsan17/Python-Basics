5e4b13a772860b1220f2c4fcc88f692e0a0f4dd6
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-06 14:02:27 +0530

    loops in python

--- example/loops.py
+++ example/loops.py
@@ -4,3 +4,12 @@ for i in range(5):
 
 for j in range(0,10,2):  #(Startvalue,Stopvalue,Stepvalue)
     print(j)
+
+x=1
+while(x<=10):
+    print("Hello World")
+    print(x)
+    x+=2
+else:
+    print("bye")    
+    
