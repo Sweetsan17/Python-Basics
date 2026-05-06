703cf40afc55748356851aeb335920064124c584
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-06 22:36:10 +0530

    sample codes testing

--- examples/file.py
+++ examples/file.py
@@ -1,10 +1,12 @@
-with open("test.txt" , "w") as f:
+with open("test.txt" , "w") as f: # w-write file and \n=break the line
     f.write("My Name is Sweetsan \n")
 
-with open("test.txt" , "a") as f:
-    f.write("My age is 20 years old")
+with open("test.txt" , "a") as f: # a-append file 
+    f.write("My age is 20 years old \n")
+    f.write("My hobby is reading a book \n")
+    f.write("My favourite color is blue")
 
-with open("test.txt", "r") as f:
+with open("test.txt", "r") as f: # r-readonly file
     content=f.read()
     print(content)
 
