332e98b5aeac9d1687b0facea765e2f8c897c6d0
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-06 15:22:12 +0530

    example folder deleted

--- example/function.py
+++
@@ -1,8 +0,0 @@
-def checkStatus():
-    Age=int(input("Enter the Age: "))
-    if Age>=18:
-        print("Adult")
-    else:
-        print("Children")
-
-checkStatus()
--- example/variables.py
+++
@@ -1,9 +0,0 @@
-num1=5
-print("Number1 =", num1)
-print(type(num1))
-num2=25.5
-print(type(num2))
-isActive=True
-print("Type of isActive is:",type(isActive))
-num3="sweetsan"
-print(type(num3))
